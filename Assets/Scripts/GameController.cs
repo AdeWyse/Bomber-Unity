3d9464903b93c20dd7932202ac7f65665db7282b
AdelineRCWG <adelinewyse@gmail.com>
2022-07-15 11:21:26 -0300

    Generation of Floor layer and 2 layer(no rules)
    Generation and criation of the initial grid and floor layer. Genaration of the second layer, without rules about object placement. Set up for selecting Character count is done.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+
+    public int numberChar;
+    public bool isControllable = true;
+    private Text numberCharDisplay;
+    private Slider numberCharSlider;
+    private Toggle controllableToggle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numberCharSlider = GameObject.Find("SliderNumberOfChar").GetComponent<Slider>();
+        numberCharDisplay = GameObject.Find("TextNumberOfChar").GetComponent<Text>();
+        controllableToggle = GameObject.Find("ToggleCrontollable").GetComponent<Toggle>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void toggleValueControl()
+    {
+        isControllable = controllableToggle.isOn;
+    }
+    public void sliderValueControl()
+    {
+        numberChar = (int)numberCharSlider.value;
+        numberCharDisplay.text = numberChar.ToString();
+    }
+}
--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -7,13 +7,21 @@ public class MapGenerator : MonoBehaviour
     public GameObject obstacle;
     public GameObject wall;
     public GameObject floor;
+    public GameObject[] characters;
     public int gridXSize = 10;
     public int gridYSize = 10;
-    public Vector3[,] grid;
+    public Vector3[,] gridPos;
+
+    private GameController gameController;
+    public int numberChar;
+    public bool isControllable = true;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        grid = new Vector3[gridXSize,gridYSize];
+        gridPos = new Vector3[gridXSize,gridYSize];
+        gameController = GameObject.Find("GameController").GetComponent<GameController>();    
     }
 
     // Update is called once per frame
@@ -23,13 +31,64 @@ public class MapGenerator : MonoBehaviour
     }
 
     public void generateMap(){
+        numberChar = gameController.numberChar;
+        if(numberChar == 0)
+        {
+            numberChar = 2;
+        }
+        isControllable = gameController.isControllable;
+        generateFloorGrid();
+        generateSecondLayer();
 
     }
 
-    public void generateFloorGrid(){
+    //Generates the second layer of objects. It includes walls, obstacles and  characters.
+   private void generateSecondLayer()
+    {
+        GameObject[] toSpawn = new GameObject[2];
+        toSpawn[0] = wall;
+        toSpawn[1] = obstacle;
+        for (int i = 0; i < gridXSize; i++)
+        {
+            for (int j = 0; j < gridYSize; j++)
+            { int random = Random.Range(0, 3);
+                var position = new Vector3(this.gridPos[i, j].x, 1.002f, this.gridPos[i, j].z);
+                if(random != 2)
+                {
+                     var spawnedObject = Instantiate(toSpawn[Random.Range(0,2)], position, Quaternion.identity);
+                }
+            }
+
+        }
+    }
+
+    //Decides how many and types of characters that will be generated
+    private GameObject[] characterSelection(int numberOfCharacters, bool isControlable)
+    {
+        GameObject[] selected = new GameObject[numberOfCharacters];
+        for(int i = numberOfCharacters - 1; i > 0; i--)
+        {
+            selected[i] = characters[0];
+        }
+        if (isControlable)
+        {
+            selected[0] = characters[1];
+        }
+        else
+        {
+            selected[0] = characters[0];
+        }
+        return selected;
+    }
+
+    //Generates the initial floor and grid location
+    private void generateFloorGrid(){
         for(int i = 0; i <gridXSize; i++){
             for(int j = 0; j <gridYSize; j++){
-                var spawnedTile = Instantiate(floor, new Vector3(x, y), Quaternion.identity);
+                var position =  new Vector3(i, 0, j);
+                var spawnedTile = Instantiate(floor, position , Quaternion.identity);
+                gridPos[i, j] = position;
+
             }
 
         }
